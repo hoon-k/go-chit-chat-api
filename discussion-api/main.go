abfbcd2496d272f12041db1d9675a52fe32401cb
Hoon <hoon.kong@pluralstack.io>
2019-06-08 16:32:03 -0700

    basic DB query

--- discussion-api/main.go
+++ discussion-api/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
 
 func main() {
+    connStr := "user=postgres password=Password1! dbname=pqgotest port=5433"
+    db, err := sql.Open("postgres", connStr)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    rows, _ := db.Query("SELECT first_name, last_name FROM users")
+    defer rows.Close()
+
+    firstName := ""
+    lastName := ""
+
+    for rows.Next() {
+        err := rows.Scan(&firstName, &lastName)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        log.Println(firstName, lastName)
+        fmt.Printf("Name is %v %v", firstName, lastName)
+    }
+
 	fmt.Printf("hello, world discussion api\n")
 }
--- user-api/main.go
+++ user-api/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+    "database/sql"
+    "fmt"
+    "log"
+
+    _ "github.com/lib/pq"
+)
 
 func main() {
+    connStr := "user=postgres password=Password1! dbname=chitchat_users port=5433 sslmode=disable"
+    db, err := sql.Open("postgres", connStr)
+    defer db.Close()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    rows, err := db.Query("SELECT first_name, last_name FROM users")
+    defer rows.Close()
+
+    var firstName string
+    var lastName string
+
+    for rows.Next() {
+        err := rows.Scan(&firstName, &lastName)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        log.Println(firstName, lastName)
+        fmt.Printf("Name is %v %v\n", firstName, lastName)
+    }
+
 	fmt.Printf("hello, world user api\n")
 }
